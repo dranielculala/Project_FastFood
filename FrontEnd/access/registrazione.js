999ab163aed04e47521bc01b7680bdd746ce70b8
draniel.culala <draniel.culala@studenti.unimi.it>
2025-07-28 14:55:10 +0200

    aggiunto modifiche visual casella rossa nei casi di errori

--- FrontEnd/access/registrazione.js
+++ FrontEnd/access/registrazione.js
@@ -47,42 +47,57 @@ function isValid() {
     const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!._-])[A-Za-z\d!._-]{8,16}$/;
     const regexUsername = /^.{3,}$/;
 
-    const cUsername = document.getElementById('username').value;
-    const cEmail = document.getElementById('email').value;
-    const cPassword = document.getElementById('password').value;
-    const cPasswordConfirm = document.getElementById('password_rep').value;
-    const cUser_type = document.getElementById('user_type').value;
+    const cUsername = document.getElementById('username');
+    const cEmail = document.getElementById('email');
+    const cPassword = document.getElementById('password');
+    const cPasswordConfirm = document.getElementById('password_rep');
+    const cUser_type = document.getElementById('user_type');
     var error = document.getElementById('alert');
 
     error.innerHTML = "Errori:"; 
 
     // Username check
-    if (!regexUsername.test(cUsername)) {
+    if (!regexUsername.test(cUsername.value)) {
         valid = false;
+        cUsername.classList.add('border', 'border-danger');
         error.innerHTML += "<br> Username non valido (deve essere lungo almeno 3 caratteri). ";
+    } else {
+        cUsername.classList.remove('border', 'border-danger');
     }
 
     // Email check
-    if (!regexEmail.test(cEmail)) {
+    if (!regexEmail.test(cEmail.value)) {
         valid = false;
+        cEmail.classList.add('border', 'border-danger');
         error.innerHTML += "<br> Email non valida.";
+    } else {
+        cEmail.classList.remove('border', 'border-danger');
     }
 
     // Password check
-    if (!regexPassword.test(cPassword)) {
+    if (!regexPassword.test(cPassword.value)) {
         valid = false;
+        cPassword.classList.add('border', 'border-danger');
         error.innerHTML += "<br> Password non valida (deve contenere almeno 8 caratteri e al massimo 16 caratteri, un maiuscolo, un minuscolo, un numero e un carattere speciale).";
+    } else {
+        cPassword.classList.remove('border', 'border-danger');
     }
 
-    if(cPassword !== cPasswordConfirm) {
+    if(cPassword.value !== cPasswordConfirm.value) {
         valid = false;
+        cPasswordConfirm.classList.add('border', 'border-danger');
         error.innerHTML += "<br> Le password non corrispondono.";
+    } else {
+        cPasswordConfirm.classList.remove('border', 'border-danger');
     }
 
     // User type check
-    if( cUser_type === "undefined") {
+    if( cUser_type.value === "undefined") {
         valid = false;
+        cUser_type.classList.add('border', 'border-danger');
         error.innerHTML += "<br> Scegliere il tipo di utente.";
+    } else {
+        cUser_type.classList.remove('border', 'border-danger');
     }
 
     return valid;
